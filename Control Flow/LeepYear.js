65e0d01f3431a8616b722d27e197e07d3e0caccc
Navya Verma <navyaverma451@gmail.com>
2026-05-22 10:54:00 +0530

    Initial commit

---
+++ Control Flow/LeepYear.js
@@ -0,0 +1,8 @@
+// Check whether a year is leep year or not 
+const prompt = require('prompt-sync')();
+let year = Number(prompt("Enter a year: "));
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    console.log(`${year} is a leap year.`);
+} else {
+    console.log(`${year} is not a leap year.`);
+}       
