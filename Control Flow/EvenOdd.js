c4f692b6e2e6f6165c9cc96292bc8bdfd09624ea
Navya Verma <navyaverma451@gmail.com>
2026-05-22 10:33:30 +0530

    Initial commit

---
+++ Control Flow/EvenOdd.js
@@ -0,0 +1,10 @@
+// 
+const prompt = require('prompt-sync')();
+
+let no = Number(prompt("Enter a number: "));
+
+if (no % 2 === 0) {
+    console.log("Even Number");
+} else {
+    console.log("Odd Number");
+}
