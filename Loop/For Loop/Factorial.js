065c36160b4546b2d31768e04bc1be24906f07a8
Navya Verma <navyaverma451@gmail.com>
2026-05-23 10:01:18 +0530

    Initial commit

---
+++ Loop/For Loop/Factorial.js
@@ -0,0 +1,11 @@
+// 6. Factorial of a Number
+const prompt = require("prompt-sync")();
+
+let n = Number(prompt("Enter a number: "));
+let factorial = 1;
+
+for (let i = 1; i <= n; i++) {
+    factorial *= i;
+}
+
+console.log("Factorial = ", factorial);
