7e852c42091aaa228ca5756ed222ee0af21ce34e
Navya Verma <navyaverma451@gmail.com>
2026-05-23 09:56:29 +0530

    Initial commit

---
+++ Loop/For Loop/EvenNumber.js
@@ -0,0 +1,8 @@
+// 2. Even Numbers
+const prompt = require('prompt-sync')();
+let n = Number(prompt('Enter the number still you want to print: '));
+for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
