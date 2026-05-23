d6a84653bb4028bfce9e9027ebcc490132ab388b
Navya Verma <navyaverma451@gmail.com>
2026-05-23 09:57:37 +0530

    Initial commit

---
+++ Loop/For Loop/Sum.js
@@ -0,0 +1,11 @@
+// 3. Sum of Numbers
+const prompt = require("prompt-sync")();
+
+let n = Number(prompt("Enter a number: "));
+let sum = 0;
+
+for (let i = 1; i <= n; i++) {
+    sum += i;
+}
+
+console.log("Sum =", sum);
