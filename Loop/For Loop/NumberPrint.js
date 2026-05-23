1cdfce1ee4934248f57b138e6c19e4cbed4c1472
Navya Verma <navyaverma451@gmail.com>
2026-05-23 09:54:15 +0530

    Initial commit

---
+++ Loop/For Loop/NumberPrint.js
@@ -0,0 +1,6 @@
+// 1. Print Numbers
+const prompt = require('prompt-sync')();
+const n = parseInt(prompt('Enter the number still you want to print: '));
+for (let i = 1; i <= n; i++) {
+    console.log(i);
+}
