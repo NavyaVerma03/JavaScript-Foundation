cab796e383206ce0cae3fa471963910c81af1728
Navya Verma <navyaverma451@gmail.com>
2026-05-28 11:17:36 +0530

    Initial commit

---
+++ Loop/While Loop/PrintEven.js
@@ -0,0 +1,6 @@
+// Print all even numbers from 1 to 20 using while loop.
+let i =2
+while(i <= 20 ){
+    console.log(i);
+    i += 2;
+}
---
+++ Loop/While Loop/PrintNumber.js
@@ -0,0 +1,6 @@
+// Use a while loop to print numbers from 1 to 10.
+let i = 1
+while(i <= 10 ){
+    console.log(i);
+    i++;
+}
